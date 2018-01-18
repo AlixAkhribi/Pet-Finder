d5973577217f41d946c512d0e383917974eb6435
Unknown <asaquilabon@gmail.com>
2018-01-18 11:28:05 -0800

    Fixed some bugs in Dash.jsx

--- src/views/Pages/LockScreenPage.jsx
+++ src/views/Pages/LockScreenPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-    FormGroup, FormControl
+  FormGroup, FormControl,
 } from 'react-bootstrap';
 
 import Button from 'elements/CustomButton/CustomButton.jsx';
@@ -8,28 +8,28 @@ import Button from 'elements/CustomButton/CustomButton.jsx';
 // This is just a default for our User's Image. We need to have it be the user's uploaded image in production //
 import avatar from 'assets/img/default-avatar.png';
 
-class LockScreenPage extends Component{
-    render(){
-        return (
-            <form className="ng-untouched ng-pristine ng-valid">
-                <div className="user-profile">
-                    <div className="author">
-                        <img alt="..." className="avatar" src={avatar} />
-                    </div>
-                    <h4>User Name</h4>
-                    <FormGroup>
-                        <FormControl
-                            type="password"
-                            placeholder="Enter Password"
-                        />
-                    </FormGroup>
-                    <Button wd neutral round>
+class LockScreenPage extends Component {
+  render() {
+    return (
+      <form className="ng-untouched ng-pristine ng-valid">
+        <div className="user-profile">
+          <div className="author">
+            <img alt="..." className="avatar" src={avatar} />
+          </div>
+          <h4>User Name</h4>
+          <FormGroup>
+            <FormControl
+              type="password"
+              placeholder="Enter Password"
+            />
+          </FormGroup>
+          <Button wd neutral round>
                         Unlock
-                    </Button>
-                </div>
-            </form>
-        );
-    }
+          </Button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default LockScreenPage;
