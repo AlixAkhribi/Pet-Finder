2feba8ffcc807cf79ed5f1b1544dc922b269593a
Unknown <AlixAkhribi@yahoo.com>
2018-01-19 13:22:39 -0800

    Added Shelter Registration Wizard Form with Validation

---
+++ src/views/Pages/ShelterRegister/ShelterStep1.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+    Row, Col,
+    FormGroup, FormControl, ControlLabel
+} from 'react-bootstrap';
+
+
+class Step1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            emailError: null,
+            shelterPhone: "",
+            password: "",
+            cfpassword: "",
+            shelterName: '',
+            shelterName_Error: null,
+            type_numberError: null,
+            
+        }
+    }
+    isValidated() {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        re.test(this.state.email) === false ? this.setState({ emailError: (<small className="text-danger">Email is required and format should be <i>john@doe.com</i>.</small>) }) : this.setState({ emailError: null });
+        return re.test(this.state.email);
+    }
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        });
+        event.target.value.length < 6 ? this.setState({ passwordError: (<small className="text-danger">You must enter a password of at least 6 characters.</small>) }) : this.setState({ passwordError: null });
+    }
+    handleCfPasswordChange(event) {
+        this.setState({
+            cfpassword: event.target.value
+        });
+        event.target.value !== this.state.password ? this.setState({ cfpasswordError: (<small className="text-danger">Passwords do not match.</small>) }) : this.setState({ cfpasswordError: null });
+    }
+    handleRegisterSubmit() {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        re.test(this.state.email) === false ? this.setState({ emailError: (<small className="text-danger">Email is required and format should be <i>john@doe.com</i>.</small>) }) : this.setState({ emailError: null });
+        this.state.password.length < 6 ? this.setState({ passwordError: (<small className="text-danger">You must enter a password of at least 6 characters.</small>) }) : this.setState({ passwordError: null });
+        this.state.cfpassword !== this.state.password ? this.setState({ cfpasswordError: (<small className="text-danger">Passwords do not match.</small>) }) : this.setState({ cfpasswordError: null });
+      }
+    render() {
+        return (
+            <div className="wizard-step">
+                <h5 className="text-center">Please tell us more about yourself.</h5>
+                <Row>
+                    <Col md={10} mdOffset={1}>
+                        <FormGroup>
+                            <ControlLabel>Shelter name</ControlLabel>
+                            <FormControl type="text" name="shelter_name" placeholder="ex: Orange County Animal Shelter" 
+                            onChange={(event) => {
+                                this.setState({ type_text: event.target.value });
+                                event.target.value === '' ? this.setState({ type_textError: (<small className="text-danger">Text is required.</small>) }) : this.setState({ type_textError: null });
+                            }}/>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={5} mdOffset={1}>
+                        <FormGroup>
+                            <ControlLabel>Password: <span className="star">*</span></ControlLabel>
+                            <FormControl type="password" name="password" onChange={(event) => this.handlePasswordChange(event)} />
+                            {this.state.passwordError}
+                        </FormGroup>
+                    </Col>
+                    <Col md={5}>
+                        <FormGroup>
+                            <ControlLabel>Confirm password: <span className="star">*</span></ControlLabel>
+                            <FormControl type="password" name="cfpassword" onChange={(event) => this.handleCfPasswordChange(event)} />
+                            {this.state.cfpasswordError}
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={7} mdOffset={1}>
+                        <FormGroup>
+                            <ControlLabel>Email <span className="text-danger">*</span></ControlLabel>
+                            <FormControl type="email" name="email" placeholder="ex: hello@creative-tim.com" onChange={(event) => this.setState({ email: event.target.value })} />
+                            {this.state.emailError}
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <FormGroup>
+                            <ControlLabel>Phone Number <span className="star">*</span></ControlLabel>
+                            <FormControl type="number" name="shelterPhone" onChange={(event) => {
+                                this.setState({shelterPhoner: event.target.value});
+                                var digitRex = /^\d+$/;
+                                digitRex.test(event.target.value) === false ? this.setState({ type_numberError: (<small className="text-danger">Has to be a number</small>) }):this.setState({ type_numberError: null });
+                            }}/>
+                            {this.state.type_numberError}
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default Step1;
---
+++ src/views/Pages/ShelterRegister/ShelterStep2.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import {
+    Row, Col,
+    FormGroup, FormControl, ControlLabel
+} from 'react-bootstrap';
+// react component that creates a dropdown menu for selection
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+import {
+    selectOptions
+} from 'variables/Variables.jsx';
+
+class Step2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Street1: "",
+            Street2: "",
+            ShelterCity: "",
+            ShelterState: "",
+            ShelterStateSelect: "",
+            shelterZipCode: "",
+            phoneNumber: "",
+            type_textError: null,
+            type_numberError: null
+        }
+    }
+    handleTypeValidation() {
+        this.state.type_text === '' ? this.setState({ type_textError: (<small className="text-danger">Text is required.</small>) }) : this.setState({ type_textError: null });
+        const digitRex = /^\d+$/;
+
+        digitRex.test(this.state.type_number) === false ? this.setState({ type_numberError: (<small className="text-danger">type_number has to be a number.</small>) }) : this.setState({ type_numberError: null });
+    }
+    render() {
+        return (
+            <div className="wizard-step">
+                <h5 className="text-center">Please give us more details about your location</h5>
+                <Row>
+                    <Col md={10} mdOffset={1}>
+                        <FormGroup>
+                            <ControlLabel>Street Name<span className="text-danger">*</span></ControlLabel>
+                            <FormControl type="text" name="Street1" placeholder="" onChange={(event) => {
+                                this.setState({ type_text: event.target.value });
+                                event.target.value === '' ? this.setState({ type_textError: (<small className="text-danger">Text is required.</small>) }) : this.setState({ type_textError: null });
+                            }}
+                            />
+                            {this.state.type_textError}
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={5} mdOffset={1}>
+                        <FormGroup>
+                            <ControlLabel>Street Name 2</ControlLabel>
+                            <FormControl type="text" name="Street1" placeholder=""
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col md={5}>
+                        <FormGroup>
+                            <ControlLabel>City<span className="text-danger">*</span></ControlLabel>
+                            <FormControl type="text" name="ShelterCity" placeholder="" onChange={(event) => {
+                                this.setState({ type_text: event.target.value });
+                                event.target.value === '' ? this.setState({ type_textError: (<small className="text-danger">Text is required.</small>) }) : this.setState({ type_textError: null });
+                            }}
+                            />
+                            {this.state.type_textError}
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={5} mdOffset={1}>
+                        <FormGroup>
+                        <ControlLabel>State</ControlLabel>
+                        <Select
+                            name="ShelterState"
+                            value={this.state.ShelterStateSelect}
+                            options={[
+                                {value: 1,label:"AL"},
+                                {value: 2,label:"AK"},
+                                {value: 3,label:"AZ"},
+                                {value: 4,label:"AR"},
+                                {value: 5,label:"CA"},
+                                {value: 6,label:"CO"},
+                                {value: 7,label:"CT"},
+                                {value: 8,label:"DE"},
+                                {value: 9,label:"FL"},
+                                {value: 10,label:"GA"},
+                                {value: 11,label:"HI"},
+                                {value: 12,label:"ID"},
+                                {value: 13,label:"IL"},
+                                {value: 14,label:"IN"},
+                                {value: 15,label:"IA"},
+                                {value: 16,label:"KS"},
+                                {value: 17,label:"KY"},
+                                {value: 18,label:"LA"},
+                                {value: 19,label:"ME"},
+                                {value: 20,label:"MD"},
+                                {value: 21,label:"MA"},
+                                {value: 22,label:"MI"},
+                                {value: 23,label:"MN"},
+                                {value: 24,label:"MS"},
+                                {value: 25,label:"MO"},
+                                {value: 26,label:"MT"},
+                                {value: 27,label:"NE"},
+                                {value: 28,label:"NV"},
+                                {value: 29,label:"NH"},
+                                {value: 30,label:"NJ"},
+                                {value: 31,label:"NM"},
+                                {value: 32,label:"NY"},
+                                {value: 33,label:"NC"},
+                                {value: 34,label:"ND"},
+                                {value: 35,label:"OH"},
+                                {value: 36,label:"OK"},
+                                {value: 37,label:"OR"},
+                                {value: 38,label:"PA"},
+                                {value: 39,label:"RI"},
+                                {value: 40,label:"SC"},
+                                {value: 41,label:"SD"},
+                                {value: 42,label:"TN"},
+                                {value: 43,label:"TX"},
+                                {value: 44,label:"UT"},
+                                {value: 45,label:"VT"},
+                                {value: 46,label:"VA"},
+                                {value: 47,label:"WA"},
+                                {value: 48,label:"WV"},
+                                {value: 49,label:"WI"},
+                                {value: 50,label:"WY"},
+                            ]}
+                            onChange={(value) => this.setState({ ShelterStateSelect: value})}
+                        />
+                        </FormGroup>
+                    </Col>
+                    <Col md={5}>
+                        <FormGroup>
+                            <ControlLabel>Zip Code <span className="text-danger">*</span></ControlLabel>
+                            <FormControl
+                                type="number"
+                                name="shelterZipCode"
+                                onChange={(event) => {
+                                    this.setState({ shelterZipCode: event.target.value });
+                                    const digitRex = /^\d+$/;
+                                    digitRex.test(event.target.value) === false ? this.setState({ type_numberError: (<small className="text-danger">type_number has to be a number.</small>) }) : this.setState({ type_numberError: null });
+                                }}
+                            />
+                            {this.state.type_numberError}
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default Step2;
